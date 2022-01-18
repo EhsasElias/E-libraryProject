9a00a2abdadf806dfe76f44c8babb2e52f64c4dc
Ehsas <ehsas.1998000@gmail.com>
2022-01-18 23:49:13 +0300

    v1 edit

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,9 +1,15 @@
 const _innerCont = document.getElementById("inneContent");
 const _loginModel = document.getElementById('login-model');
 const _login = document.getElementById('login');
+const _signup = document.getElementById('signup');
+const _createAccount = document.getElementById('createAccount');
 
 _login.addEventListener('click' , ()=>{
     _innerCont.style.display = "none";
     _loginModel.style.display="flex";
 
+});
+_createAccount.addEventListener('click' , ()=>{
+_loginModel.style.display = "none";
+_signup.style.display ="block";
 });
